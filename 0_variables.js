343d4580e38cf9c9bdc076be1fc63774dfc3ae86
SKMODANWAL-debug <sumitkumarmodanwal5555@gmail.com>
2025-03-21 10:32:27 +0530

    first commit

---
+++ 0_variables.js
@@ -0,0 +1,18 @@
+var my_firstName = "John";
+
+console.log(my_firstName$);
+
+var _myLastName$ = "Doe";
+
+console.log(_myLastName$);
+
+var 123myAge = 25;
+console.log(123myAge);
+
+var $cityName = "New York";
+console.log($cityName);
+
+var my@Email = "John@example.com";
+console.log(my@gmail);
+
+
---
+++ 1_data.types.js
@@ -0,0 +1,32 @@
+//* data types define the type of the values that a variables can hold
+//? types of primitive data types 
+
+//? Number: represents numeric values, including integers and floating-point numbers.
+
+// Example:
+
+var my FavNum = -5;
+
+console.log(my FavNum);
+
+//? string: represents a sequence of character inclosed in single or double quantities
+
+// Example:
+   var myname = sumit ;
+   console.log(MyName);
+
+
+//? Boolean represents a logical entity with two  values: true or false.
+
+// Example 
+
+var isRaining = false;
+var are you Awesome = true;
+console.log(isRaining)
+
+//? undefined Represents the absence of a value or an uninitialize variables.
+
+// Example:
+
+
+//? null: represents the absence of a value.r
